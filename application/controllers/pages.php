0b5da8e96f144860b65eaf9f4c0d45cf15aa2e09
Shivabeach <mrwilson1@comcast.net>
2015-08-07 21:13:27 -0400

    added model

---
+++ application/controllers/pages.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Pages extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->database();
+		$this->load->model("get_data");
+		$data["results"] = $this->get_data->getdata("vanhorn");
+		$data1['title'] = "Main Page";
+		$this->load->view("head/head", $data1);
+		$this->load->view("include/nav");
+		$this->load->view('include/main', $data);
+		$this->load->view("foot/footer");
+
+	}
+
+	public function fam()
+	{
+		$data['title'] = 'Family Insertion';
+		$this->load->view('admin/family-insert', $data);
+	}
+
+}
+
+/* End of file pages.php */
+/* Location: ./application/controllers/pages.php */
